bd33e5c975cf65f0bd64f07bfc67b6fb928997b0
savvy1525 <savvypostal@outlook.com>
2024-01-15 12:00:50 +0100

    changed upstream for upstream branch

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -3,7 +3,7 @@ package com.houarizegai.calculator;
 import com.houarizegai.calculator.ui.CalculatorUI;
 
 public class App {
-    //Upstream BranchS
+    //Upstream Branch
     public static void main(String[] args) {
         new CalculatorUI();
     }
